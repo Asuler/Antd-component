3b383a67eebcd6d7dcecee6238c2022c5ab44f00
王佰庆 <1289368293@qq.com>
2019-09-06 11:55:15 +0800

    1

---
+++ utils/mergeDeep.js
@@ -0,0 +1,45 @@
+function mergeDeep(originalValue, targetValue) {
+  if (isArray(originalValue)) {
+    return targetValue;
+  }
+
+  if (!isObject(originalValue)) {
+    return targetValue;
+  }
+
+  if (typeof originalValue !== typeof targetValue) {
+    return targetValue;
+  }
+
+  if (originalValue === null || targetValue === null) {
+    return targetValue;
+  }
+
+  if ((originalValue.constructor && originalValue.constructor.name) === 'Moment') {
+    return targetValue;
+  }
+
+  const originalValueCopy = { ...originalValue };
+
+  for (const name in originalValueCopy) {
+    if (name in targetValue) {
+      originalValueCopy[name] = mergeDeep(originalValueCopy[name], targetValue[name]);
+    }
+  }
+
+  for (const name in targetValue) {
+    if (!(name in originalValueCopy)) {
+      originalValueCopy[name] = targetValue[name];
+    }
+  }
+
+  return originalValueCopy;
+}
+
+export default mergeDeep
+
+
+// 使用方法-redux中的reducer,深度合并
+// mergeState(state, { payload }) {
+//   return mergeDeep(state, payload);
+//}
